2d0e95de4b05322d602ca970a73b6a5265a9afaa
au472091 <simeonqs@hotmail.com>
2023-09-22 16:19:22 +0200

    Updating run_amplitude_threshold.R. Adding function to export detections to wav. Also minor edits.

--- analysis/code/run_amplitude_threshold.R
+++ analysis/code/run_amplitude_threshold.R
@@ -15,12 +15,24 @@ library(parallel)
 # List paths
 path_files = '../../../NA14_A_first_half'
 path_detections_txt = 'analysis/results/NA14_A_first_half/detections_txt/'
+path_wavs = 'analysis/results/NA14_A_first_half/wavs/'
 
 # List files to detect on 
 files = list.files(path_files, recursive = TRUE, full.names = TRUE)
-# files = files[1:100]
+files = files[1:100]
 
-# Function to proces file
+# Function to export selection to wav
+export.detections.to.wav = function(wave, detections, base_path_out){
+  if(nrow(detections) > 0){
+    lapply(seq_len(nrow(detections)), function(i)
+      writeWave(wave[detections$start[i]:detections$end[i]],
+                sprintf('%s_%s_%s_%s.wav', base_path_out, i, 
+                        detections$start[i], detections$end[i]),
+                extensible = FALSE))
+  }
+}
+
+# Function to process file
 process.file = function(file){
   
   # Get file name for storing
@@ -28,15 +40,17 @@ process.file = function(file){
   
   # Run amplitude threshold on one file
   wave = readWave(file)
-  wave = ffilter(wave, from = 10000, output = 'Wave')
-  detections = call.detect.multiple(wave, threshold = 0.045, min_dur = 0,
-                                    plot_it = FALSE,
+  wave = ffilter(wave, from = 10000, to = 90000, 
+                 output = 'Wave')
+  wave = downsample(wave, 192000)
+  detections = call.detect.multiple(wave, threshold = 0.065, min_dur = 0,
                                     save_extra = 0.01, env_type = 'summed',
-                                    bin_depth = 256, merge_overlap = TRUE)
-  
+                                    bin_depth = 128, merge_overlap = TRUE)
+
   # Export detections
   export.detections(detections, wave@samp.rate,
                     sprintf('%s%s.txt', path_detections_txt, file_short))
+  base_path_out = sprintf('%s%s', path_wavs, file_short)
   
 } # end process.file
 
