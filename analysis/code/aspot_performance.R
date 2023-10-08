87b88f5629aeb511b0b6d999b2cfac6596259a2c
au472091 <simeonqs@hotmail.com>
2023-10-08 09:12:48 +0200

    Updating aspot_performance.R. Adding n_calls, n_fp and n_fn columns to overview.

--- analysis/code/aspot_performance.R
+++ analysis/code/aspot_performance.R
@@ -16,7 +16,7 @@ rm(list=ls())
 
 # Paths 
 path_gt = 'analysis/results/test_data/ground_truth_selection_tables'
-path_d = 'aspot/m03/selection_tables'
+path_d = 'aspot/m16/selection_tables'
 path_performance = 'analysis/results/test_data/performance_aspot/'
 
 # List files
@@ -25,6 +25,9 @@ d_files = list.files(path_d, full.names = TRUE)
 
 # Function to process file
 process.file = function(path){
+  
+  # Print path
+  print(path)
   
   # Load detections
   d = load.selection.table(path)
@@ -61,6 +64,41 @@ process.file = function(path){
   
 } # end process.file
 
-# Create overview
+# Calculate performance
 out = lapply(d_files, process.file)
+
+# Make summary
+perf_files = list.files(path_performance, pattern = '*.txt', full.names = TRUE)
+n_calls = perf_files |> basename() |> str_remove('.txt') |> 
+  vapply(function(file) 
+    load.selection.table(gt_files[str_detect(gt_files, file)]) |> nrow(), 
+    numeric(1))
+perf_overview = data.frame(file = basename(perf_files),
+                           n_calls = n_calls,
+                           n_fp = numeric(length(perf_files)),
+                           n_fn = numeric(length(perf_files)),
+                           fp_rate = numeric(length(perf_files)),
+                           fn_rate = numeric(length(perf_files)))
+for(i in seq_along(perf_files)){
+  perf = read.table(perf_files[i], sep = ';')
+  l1 = which(perf == '$fp')
+  l2 = which(perf == '$fn')
+  l3 = which(perf == '$fp_rate')
+  l4 = which(perf == '$fn_rate')
+  # if n_fp is NULL, return zero, else split into all detections and subtract
+  # 1, because the string also includes [1] 
+  perf_overview$n_fp[i] = ifelse(perf[l1+1,] == 'NULL', 0, 
+                                 length(str_split(perf[l1+1,], ' ')[[1]]) - 1)
+  perf_overview$n_fn[i] = ifelse(perf[l2+1,] == 'NULL', 0, 
+                                 length(str_split(perf[l2+1,], ' ')[[1]]) - 1)
+  perf_overview$fp_rate[i] = perf[l3+1,] |> 
+    str_remove('\\[1\\] ') |> 
+    as.numeric() |> round(3)
+  perf_overview$fn_rate[i] = perf[l4+1,] |> 
+    str_remove('\\[1\\] ') |> 
+    as.numeric() |> round(3)
+}
+write.csv(perf_overview, sprintf('%s0_summary.csv', path_performance))
+
+# Message
 message('Stored performance files.')
