871a7d7c8b7ffab25934e9be99694ee63c34166f
Simeon Q. Smeele <simeonqs@hotmial.com>
2023-11-20 09:36:24 +0100

    Submitting lots of no changes. Not sure what this is.

--- analysis/code/aspot_plot_specs_detections.R
+++ analysis/code/aspot_plot_specs_detections.R
@@ -21,10 +21,10 @@ resample_rate = 192000
 wing = 0.01 # how much to add before and after detection
 
 # Paths 
-path_detections = 'aspot/models/m40/selection_tables_T3'
+path_detections = 'aspot/models/m45/selection_tables_NS31_B_Spring23'
 path_audio = 
-  '/media/au472091/T7 Shield/LOT_1_BØJER_DATA/T3-NS26_A_Spring23/Data'
-path_pdf = 'aspot/models/m40/specs_detections_T3'
+  '/media/au472091/T7 Shield/LOT_1_BØJER_DATA/NS31_B_Spring23/Data'
+path_pdf = 'aspot/models/m45/specs_detections_NS31_B_Spring23'
 
 # Load selection tables
 detections = load.selection.tables(path_detections)
@@ -32,20 +32,20 @@ detections = load.selection.tables(path_detections)
 # Function to plot specs
 plot.spec = function(detection_row, path_audio = NULL, path_pdf = NULL){
   
-  if(!is.null(path_pdf)) pdf(sprintf('%s/%s_%s.pdf', 
-                                     path_pdf, 
-                                     detection_row$file,
-                                     detection_row$Selection))
+  # if(!is.null(path_pdf)) pdf(sprintf('%s/%s_%s.pdf', 
+  #                                    path_pdf, 
+  #                                    detection_row$file,
+  #                                    detection_row$Selection))
   wave = readWave(sprintf('%s/%s.wav', path_audio, detection_row$file), 
                   from = detection_row$Begin.time..s. - wing, 
                   to = detection_row$End.time..s. + wing,
                   units = 'seconds')
-  better.spectro(wave, xlim = c(0, 0.05))
-  abline(v = c(wing, 
-               detection_row$End.time..s. - 
-                 detection_row$Begin.time..s. + wing),
-         lty = 2, lwd = 2)
-  if(!is.null(path_pdf)) dev.off()
+  # better.spectro(wave, xlim = c(0, 0.05))
+  # abline(v = c(wing, 
+  #              detection_row$End.time..s. - 
+  #                detection_row$Begin.time..s. + wing),
+  #        lty = 2, lwd = 2)
+  # if(!is.null(path_pdf)) dev.off()
   writeWave(wave, sprintf(sprintf('%s/%s_%s.wav', 
                                   path_pdf, 
                                   detection_row$file,
