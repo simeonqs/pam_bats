cdd59b3c6db390d1b1cd484329778066be78b9eb
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-01-09 10:08:37 +0100

    Updating aspot_downsample.R. Adding debug option.

--- analysis/code/aspot_downsample.R
+++ analysis/code/aspot_downsample.R
@@ -19,14 +19,16 @@ rm(list=ls())
 
 # Paths 
 path_wavs = 
-  '/home/au472091/Documents/large_data/LOT_1_BØJER_DATA/NS21_A_Spring23/Data'
+  '/media/au472091/T7 Shield/LOT_1_BØJER_DATA/NS13_A_Spring23/Data'
 path_results = 
-  '/home/au472091/Documents/large_data/downsampled/NS21_A_Spring23'
-path_predictions = ''
-  
+  '/media/au472091/T7 Shield/downsampled_data/NS13_A_Spring23'
+path_predictions = 
+  '/home/au472091/Documents/results_aspot/NS13_A_Spring23/predict'
+
 # Settings
 resample_rate = 192000
 bandpass = c(1000, 95000)
+debug = TRUE
 
 # List audio files
 files = list.files(path_wavs, '*wav', recursive = FALSE, full.names = TRUE)
@@ -42,6 +44,7 @@ files = files[!basename(files) %in% c(already_done, already_predicted)]
 
 # Function to process file
 downsample.file = function(file){
+  if(debug) print(file)
   wave = readWave(file)
   orig_max = max(abs(wave@left))
   wave = ffilter(wave, from = bandpass[1], to = bandpass[2], 
@@ -53,6 +56,7 @@ downsample.file = function(file){
 
 # Run on all files
 message(sprintf('Downsampling %s files...', length(files)))
-mclapply(files, downsample.file, mc.cores = 15)
+if(!debug) mclapply(files, downsample.file, mc.cores = 15)
+if(debug) lapply(files, downsample.file)
 message('Downsampled all files.')
 
