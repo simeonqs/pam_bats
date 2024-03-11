c098dd397d0b0c7ca70cd6bc70937260f9d8bcc5
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-03-11 16:07:18 +0100

    Updating conf_matrix_aspot_sono.R. Major rewrite. Now outputting three plots: aspot vs sono, aspot vs manual and sono vs manual.

--- analysis/code/conf_matrix_aspot_sono.R
+++ analysis/code/conf_matrix_aspot_sono.R
@@ -15,143 +15,54 @@ for(lib in libraries){
 rm(list=ls()) 
 
 # Paths 
-path_sono = 'analysis/data/sonochiro/results.csv'
-path_aspot = 
-  '~/Documents/large_data/results_aspot/defenitely_bats/NS30_B_Fall2023'
-path_pdf = 'analysis/results/confusion_matrix_sonochiro_aspot.pdf'
+path_sono = 'analysis/results/sonochiro/results.csv'
+path_aspot = '~/Documents/results_aspot/defenitely_bats/fall'
+path_ground_truth = 'aspot/test_data_sets/boeje_test_data/txt'
+path_png = 'analysis/results/confusion_matrix_sonochiro_aspot.png'
+path_functions = 'analysis/code/functions'
+
+# Load functions
+.functions = lapply(list.files(path_functions, full.names = TRUE), source)
 
 # Load data
 sono = read.csv(path_sono)
-aspot = load.selection.tables(path_aspot)
+aspot = load.selection.tables(path_aspot, recursive = TRUE)
+manual = load.selection.tables(path_ground_truth)
+
+# Subset to include only folders that are shared
+sono$folder = sono$file |> strsplit('_') |> sapply(`[`, 1)
+aspot$folder = aspot$file |> strsplit('_') |> sapply(`[`, 1)
+manual$folder = manual$file |> strsplit('_') |> sapply(`[`, 1)
+intersect_folders = intersect(intersect(sono$folder, aspot$folder), 
+                              manual$folder)
+aspot = aspot[aspot$folder %in% intersect_folders,]
+manual = manual[manual$folder %in% intersect_folders,]
+sono = sono[sono$folder %in% intersect_folders,]
 
 # Subset sono to only files with bat detections
-sono = sono[sono$id_final %in% c('ENVsp', 'Pipnat', 'Pipnat m feeding buzz'),]
+sono = sono[!sono$Sound.type %in% 
+              c('Regn', 'Skib', 'Skib og meget støj', 'Noise', 'noise',
+                'parasi'),]
+message('Including following types from Sonochiro:')
+print(table(sono$Sound.type))
 rownames(sono) = seq_len(nrow(sono))
-sono$id_final = 'target'
+sono$Sound.type = 'target'
 
-# Function to calculate overlap
-calc.iou = function(st_d, st_g, end_d, end_g){
-  union = max(end_d, end_g) - min(st_d, st_g)
-  intercept = min(end_d, end_g) - max(st_d, st_g)
-  if(intercept > union) stop('Error in calc.overlap.')
-  return(intercept/union)
-}
-
-# Create place holders for output
-fps = fns = tps = c()
-class_results = data.frame()
-
-# List unique files
-files = unique(aspot$file)
-
-# Run through images
-for(file in files){
-  
-  ## subset
-  d = aspot[aspot$file == file,]
-  g = sono[sono$file == file,]
-  
-  ## run through detections and sonochiros
-  links = data.frame(row_d = numeric(),
-                     row_g = numeric())
-  for(row_d in rownames(d)){
-    for(row_g in rownames(g)){
-      links = rbind(links,
-                    data.frame(file = file,
-                               row_d = row_d, 
-                               row_g = row_g, 
-                               d = aspot[row_d,]$Sound.type,
-                               g = sono[row_g,]$id_final,
-                               iou = calc.iou(d[row_d,]$Begin.time..s.,
-                                              g[row_g,]$Begin.time..s.,
-                                              d[row_d,]$End.time..s.,
-                                              g[row_g,]$End.time..s.)))
-    }
-  }
-  
-  ## only keep if some overlap
-  links = links[links$iou > 0,]
-  
-  ## run through sonochiros and remove all but one link
-  ## first order so that the ones with highest overlap are at the top
-  ## then remove duplications (which will not be the top entry)
-  if(nrow(links) > 1){
-    links = links[order(links$iou, decreasing = TRUE),]
-    links = links[!duplicated(links$row_g),]
-    links = links[!duplicated(links$row_d),]
-  }
-  
-  ## check if there are any duplications left
-  if(any(duplicated(links$row_d)) | any(duplicated(links$row_g)))
-    stop('Found duplications in file ', image, '.')
-  
-  ## store remaining
-  class_results = rbind(class_results,
-                        links)
-  
-  ## get fps
-  new_fps = rownames(d)[!rownames(d) %in% links$row_d]
-  fps = c(fps, new_fps)
-  
-  ## get fns
-  new_fns = rownames(g)[!rownames(g) %in% links$row_g]
-  fns = c(fns, new_fns)
-  
-} # end image loop
-
-# Add false positives as such
-class_results = rbind(class_results,
-                      data.frame(file = aspot[fps,]$file,
-                                 row_d = fps,
-                                 row_g = 'NA',
-                                 d = aspot[fps,]$Sound.type,
-                                 g = 'noise',
-                                 iou = NA))
-
-# Add false negatives as such
-if(length(fns) > 0)
-  class_results = rbind(class_results,
-                        data.frame(file = sono[fns,]$file,
-                                   row_d = 'NA',
-                                   row_g = fns,
-                                   d = 'noise',
-                                   g = sono[fns,]$id_final,
-                                   iou = NA))
-
-# Run checks
-if(any(duplicated(class_results$row_d[class_results$row_d != 'NA']))) 
-  stop('Duplications in row d.')
-if(any(duplicated(class_results$row_g[class_results$row_g != 'NA']))) 
-  stop('Duplications in row g.')
-if(any(!rownames(aspot) %in% class_results$row_d)) 
-  stop('Missing rows from Animal Spot.')
-if(any(!rownames(sono[sono$Multiple != 'empty',]) %in% 
-       class_results$row_g)) 
-  stop('Missing rows from sonochiro.')
+# Run conf.mat
+class_results_compare = conf.mat(aspot, sono)
+class_results_aspot = conf.mat(aspot, manual)
+class_results_sono = conf.mat(sono, manual)
 
 # Plot confusion matrix
-# pdf(path_pdf)
-levels = c('target', 'noise')
-conf_matrix = table(factor(class_results$d, levels = levels), 
-                    factor(class_results$g, levels = levels))
-percentages = conf_matrix
-for(i in seq_len(nrow(percentages))) 
-  percentages[,i] = percentages[,i]/sum(percentages[,i]) * 100
-color_gradient = colorRampPalette(c('lightblue', 'darkblue'))
-plot(seq_along(levels), type = 'n', xlab = 'aspot', ylab = 'sonochiro',
-     xlim = c(0.5, length(levels)+0.5), ylim = c(0.5, length(levels)+0.5),
-     xaxt = 'n', yaxt = 'n')
-for(i in seq_along(levels)){
-  for(j in seq_along(levels)){
-    rect(i - 0.5, j - 0.5, i + 0.5, j + 0.5, 
-         col = color_gradient(101)[as.numeric(percentages[i, j]+1)])
-    text(i, j, labels = conf_matrix[i, j], col = 'white', cex = 1.5)
-  }
-}
-mtext(rownames(conf_matrix), side = 2, at = seq_along(levels), las = 2, 
-      line = 0.75)
-mtext(colnames(conf_matrix), side = 1, at = seq_along(levels), line = 0.75)
-# dev.off()
+png(path_png, width = 8.5, height = 3, units = 'in', res = 1000)
+par(mfrow = c(1, 3))
+plot.conf.mat(levels = c('target', 'noise'), class_results_compare,
+              path_pdf = NULL, labs = c('aspot', 'sono'), main = 'compared')
+plot.conf.mat(levels = c('target', 'noise'), class_results_aspot,
+              path_pdf = NULL, labs = c('aspot', 'manual'), main = 'aspot')
+plot.conf.mat(levels = c('target', 'noise'), class_results_sono,
+              path_pdf = NULL, labs = c('sono', 'manual'), main = 'sonochiro')
+dev.off()
 
-
-
+# Message
+message('Stored confusion matrix.')
