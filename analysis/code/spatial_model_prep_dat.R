6cc83a65abbbf2434f57bbdf93039b1d53ae6d9c
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-03-22 15:37:04 +0100

    Updating spatial_model_pre_dat.R. Removing some stations, only keeping non-stranded bouys from fall 2023. Increasing date range. Adding weather. Adding distance matrix.

--- analysis/code/spatial_model_prep_dat.R
+++ analysis/code/spatial_model_prep_dat.R
@@ -6,7 +6,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
-libraries = c('stringr', 'dplyr', 'lubridate')
+libraries = c('stringr', 'dplyr', 'lubridate', 'geosphere', 'callsync')
 for(lib in libraries){
   if(! lib %in% installed.packages()) lapply(lib, install.packages)
   lapply(libraries, require, character.only = TRUE)
@@ -19,12 +19,19 @@ rm(list=ls())
 path_detections = '/home/au472091/Documents/results_aspot/defenitely_bats'
 path_meta = 'analysis/data/meta_data_boejer.csv'
 path_dat_model = 'analysis/results/spatial_model/dat_model.RData'
+path_weather = 'analysis/data/Hvide Sande 2023-08-17 to 2023-09-16.csv'
 
 # List all files with detections
 files = list.files(path_detections, '*txt', recursive = TRUE)
 
 # Remove STRANDET
 files = files[!str_detect(files, 'STRAND')]
+
+# Only fall
+files = files[str_detect(files, 'Fall')]
+
+# Remove HR
+files = files[!str_detect(files,'HR3_4')]
 
 # Get night-dates
 dates = files |> basename() |> str_extract('\\d{8}')
@@ -70,12 +77,14 @@ stations =
 
 # Combine data
 dat = data.frame(date = nights$modified_date,
-                 station = stations)
+                 station = as.factor(stations))
 dat = unique(dat)
 
 # Get missing dates
 min_date = min(dat$date)
+min_date = as.Date('2023-08-10')
 max_date = max(dat$date)
+max_date = as.Date('2023-09-20')
 dat_model = data.frame()
 for(date in seq(min_date, max_date, by = 'day')){
   for(station in unique(meta$Station.ID)){
@@ -92,8 +101,41 @@ for(date in seq(min_date, max_date, by = 'day')){
 dat_model = merge(dat_model, meta, by.x = 'station', by.y = 'Station.ID',
                   all.x = TRUE, all.y = FALSE)
 
+# Add weather
+weather = read.csv(path_weather)
+
+# Extract weather of interest
+dat_model$datetime = paste0(dat_model$date, 'T21:00:00')
+dat_model = merge(dat_model, weather[c('datetime', 'temp')],
+                  all.x = TRUE, all.y = FALSE)
+
+# Number day of year
+dat_model$night_of_year = as.numeric(dat_model$date) - 
+  as.numeric(as.Date('2023-01-01'))
+
+# Translate station to numeric
+trans_stations = seq_along(unique(dat_model$station))
+names(trans_stations) = sort(unique(dat_model$station))
+dat_model$station_numeric = trans_stations[dat_model$station]
+
+# Make distance matrix per station
+combs = combn(trans_stations, 2)
+out = sapply(seq_len(ncol(combs)), function(x) {
+  st_1 = names(trans_stations)[combs[1,x]]
+  l_1 = dat_model[c('Long..E.', 'Lat..N.')][
+    dat_model$station == st_1,][1,]
+  st_2 = names(trans_stations)[combs[2,x]]
+  l_2 = dat_model[c('Long..E.', 'Lat..N.')][
+    dat_model$station == st_2,][1,]
+  return( distm(l_1, 
+                l_2, 
+                fun = distHaversine) )
+})
+out = out/max(out)
+d_mat = o.to.m(out, names(trans_stations))
+
 # Store data
-save(dat_model, file = path_dat_model)
+save(dat_model, trans_stations, d_mat, file = path_dat_model)
 
 # Message
 message('Stored data for model.')
