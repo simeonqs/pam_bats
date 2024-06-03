d23fc33f644fdd3c134e1c346614f4cc5fd47a31
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-06-03 09:06:47 +0200

    Updating activity_overview_plot.R. Removing season, now plotting Y1.

--- analysis/code/activity_overview_plot.R
+++ analysis/code/activity_overview_plot.R
@@ -15,11 +15,14 @@ for(lib in libraries){
 rm(list=ls()) 
 
 # Paths 
-path_summaries = 'analysis/results/activity_overview/summaries/summaries'
-path_detections = 'analysis/results/activity_overview/summaries/detections'
-path_aspot = 'analysis/results/activity_overview/summaries/aspot'
-path_aspot_bats = 'analysis/results/activity_overview/summaries/aspot_bats'
-path_png = 'analysis/results/activity_overview'
+path_summaries = 
+  'analysis/results/activity_overview/summaries_backup/summaries'
+path_detections = 
+  'analysis/results/activity_overview/summaries_backup/detections'
+path_aspot = 'analysis/results/activity_overview/summaries_backup/aspot'
+path_aspot_bats = 
+  'analysis/results/activity_overview/summaries_backup/aspot_bats'
+path_png = 'analysis/results/activity_overview/activity_overview'
 
 # Plotting function
 points_custom <- function(x, y, shape, col = 'black', cex = 1, ...) {
@@ -128,76 +131,69 @@ names(trans_stations) = unique_stations
 ## create colour gradient
 colfunc = colorRampPalette(c('#FAD7A0', '#0B5345'))
 cols = colfunc(max(summary$n))
-for(season in c('Forår 2023', 'Efterår 2023')){
-  pdf(sprintf('%s/%s_bøjer.pdf', path_png, str_replace(season, ' ', '_')),
-      width = 15, height = 12) # , units = 'in', res = 1000
-  # season = 'Forår 2023'
-  par(mar = c(5, 7, 3, 1))
-  ## subset per season and adjust xlims
-  if(season == 'Forår 2023'){
-    sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') <= 
+pdf(sprintf('%s_bøjer.pdf', path_png),
+    width = 40, height = 12) # , units = 'in', res = 1000
+par(mar = c(5, 7, 1, 1))
+## subset per season and adjust xlims
+if(TRUE){
+  sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') < 
+                        as.Date('2024-04-10')),]
+  sub_detections = 
+    summary_detections[which(as.Date(summary_detections$date, 
+                                     format = '%Y-%m-%d') < 
+                               as.Date('2024-04-10')),]
+  sub_aspot = 
+    summary_aspot[which(as.Date(summary_aspot$DATE) < 
+                          as.Date('2024-04-10')),]
+  sub_aspot_bats = 
+    summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) < 
+                               as.Date('2024-04-10')),]
+  xlim = as.Date(c('2023-04-10', '2024-04-10'))
+} else {
+  sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') >= 
+                        as.Date('2023-07-15')),]
+  sub_detections = 
+    summary_detections[which(as.Date(summary_detections$date, 
+                                     format = '%Y-%m-%d') >= 
+                               as.Date('2023-07-15')),]
+  sub_aspot = 
+    summary_aspot[which(as.Date(summary_aspot$DATE) >= 
                           as.Date('2023-07-15')),]
-    sub_detections = 
-      summary_detections[which(as.Date(summary_detections$date, 
-                                       format = '%Y-%m-%d') <= 
-                                 as.Date('2023-07-15')),]
-    sub_aspot = 
-      summary_aspot[which(as.Date(summary_aspot$DATE) <= 
-                            as.Date('2023-07-15')),]
-    sub_aspot_bats = 
-      summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) <= 
-                                 as.Date('2023-07-15')),]
-    xlim = as.Date(c('2023-04-10', '2023-06-30'))
-  } else {
-    sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') > 
-                          as.Date('2023-07-15')),]
-    sub_detections = 
-      summary_detections[which(as.Date(summary_detections$date, 
-                                       format = '%Y-%m-%d') > 
-                                 as.Date('2023-07-15')),]
-    sub_aspot = 
-      summary_aspot[which(as.Date(summary_aspot$DATE) > 
-                            as.Date('2023-07-15')),]
-    sub_aspot_bats = 
-      summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) > 
-                                 as.Date('2023-07-15')),]
-    xlim = as.Date(c('2023-07-30', '2023-11-15'))
-  }
-  ## create empty plot
-  plot(as.Date(sub$DATE, format = '%Y-%b-%d'),
-       trans_stations[sub$station],
-       ylim = c(min(trans_stations) - 0.5, max(trans_stations) + 0.5),
-       xlim = xlim,
-       xaxt = 'n', yaxt = 'n', type = 'n',
-       xlab = 'Dato', ylab = '', main = season)
-  mtext('Station', 2, 5)
-  ## add filled squares and colour by activity
-  for(i in seq_len(nrow(sub))){
-    points_custom(as.Date(sub$DATE[i], format = '%Y-%b-%d'),
-                  trans_stations[sub$station[i]], 
-                  my_shape1, col = cols[sub$n[i]])
-  }
-  ## add scaled dots for number detections
-  points(as.Date(sub_detections$date, format = '%Y-%m-%d'),
-         trans_stations[sub_detections$station] + 0.15, pch = 16, 
-         cex = log10(sub_detections$n)/4 + 0.1)
-  ## add scaled dots for number detections from aspot
-  points(as.Date(sub_aspot$DATE),
-         trans_stations[sub_aspot$station] - 0.15, pch = 16, 
-         cex = log10(sub_aspot$n)/4 + 0.1)
-  points(as.Date(sub_aspot_bats$DATE),
-         trans_stations[sub_aspot_bats$station] - 0.15, pch = 16, 
-         cex = log10(sub_aspot_bats$n)/4 + 0.1, col = '#28B463')
-  ## add axes
-  unique_months = unique(format(ymd(sub$DATE), '%Y-%m'))
-  axis(1, at = as.Date(paste0(unique_months, '-01')), 
-       labels = paste0(unique_months, '-01') |> str_sub(6, 10))
-  axis(1, at = as.Date(paste0(unique_months, '-10')), 
-       labels = paste0(unique_months, '-10') |> str_sub(6, 10))
-  axis(1, at = as.Date(paste0(unique_months, '-20')), 
-       labels = paste0(unique_months, '-20') |> str_sub(6, 10))
-  axis(2, trans_stations, names(trans_stations), las = 1)
-  dev.off() # close PNG
+  sub_aspot_bats = 
+    summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) >= 
+                               as.Date('2023-07-15')),]
+  xlim = as.Date(c('2023-07-30', '2023-11-15'))
 }
+## create empty plot
+plot(as.Date(sub$DATE, format = '%Y-%b-%d'),
+     trans_stations[sub$station],
+     ylim = c(min(trans_stations) - 0.5, max(trans_stations) + 0.5),
+     xlim = xlim,
+     xaxt = 'n', yaxt = 'n', type = 'n',
+     xlab = 'Dato', ylab = '')
+mtext('Station', 2, 5)
+## add filled squares and colour by activity
+for(i in seq_len(nrow(sub))){
+  points_custom(as.Date(sub$DATE[i], format = '%Y-%b-%d'),
+                trans_stations[sub$station[i]], 
+                my_shape1, col = cols[sub$n[i]])
+}
+## add scaled dots for number detections
+points(as.Date(sub_detections$date, format = '%Y-%m-%d'),
+       trans_stations[sub_detections$station] + 0.15, pch = 16, 
+       cex = log10(sub_detections$n)/4 + 0.1)
+## add scaled dots for number detections from aspot
+# points(as.Date(sub_aspot$DATE),
+#        trans_stations[sub_aspot$station] - 0.15, pch = 16, 
+#        cex = log10(sub_aspot$n)/4 + 0.1)
+points(as.Date(sub_aspot_bats$DATE),
+       trans_stations[sub_aspot_bats$station] - 0.15, pch = 16, 
+       cex = log10(sub_aspot_bats$n)/4 + 0.1, col = '#28B463')
+## add axes
+unique_months = unique(format(ymd(sub$DATE), '%Y-%m'))
+axis(1, at = as.Date(paste0(unique_months, '-01')), 
+     labels = paste0(unique_months, '-01'))
+axis(2, trans_stations, names(trans_stations), las = 1)
+dev.off() # close PNG
 
 
