e5e159efe52a315f5ce34a4f68942bfa7ccbce2c
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-06-18 14:24:08 +0200

    Updating activity_overview_plot_land.R. PNG instead of PDF. Commenting out unused code. Fixing recordings with TWJ prefix. Plotting per year rather than season.

--- analysis/code/activity_overview_plot_land.R
+++ analysis/code/activity_overview_plot_land.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: pam_bats  
 # Author: Simeon Q. Smeele
-# Description: Plots overview of active periods and detections in pdf. 
+# Description: Plots overview of active periods and detections in png. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
@@ -17,9 +17,9 @@ rm(list=ls())
 # Paths 
 path_summaries = 'analysis/results/activity_overview/summaries/summaries'
 path_detections = 'analysis/results/activity_overview/summaries/detections'
-path_aspot = 'analysis/results/activity_overview/summaries/aspot'
+# path_aspot = 'analysis/results/activity_overview/summaries/aspot'
 path_aspot_bats = 'analysis/results/activity_overview/summaries/aspot_bats'
-path_png = 'analysis/results/activity_overview'
+path_png = 'analysis/results/activity_overview/activity_overview_'
 
 # Plotting function
 points_custom <- function(x, y, shape, col = 'black', cex = 1, ...) {
@@ -47,33 +47,33 @@ files_summaries = list.files(path_summaries, pattern = '*.csv',
                              recursive = TRUE, full.names = TRUE)
 files_summaries = files_summaries[!str_detect(files_summaries, 'NS') & 
                                     !str_detect(files_summaries, 'HR') &
-                                    !str_detect(files_summaries, 'TWJ') &
+                                    # !str_detect(files_summaries, 'TWJ') &
                                     !str_detect(files_summaries, 'BOARD')]
 files_detections = list.files(path_detections, pattern = '*.csv', 
                               recursive = TRUE, full.names = TRUE)
 files_detections = files_detections[!str_detect(files_detections, 'NS') & 
                                       !str_detect(files_detections, 'HR') &
-                                      !str_detect(files_detections, 'TWJ') &
+                                      # !str_detect(files_detections, 'TWJ') &
                                       !str_detect(files_detections, 'BOARD')]
-files_aspot = list.files(path_aspot, pattern = '*.csv', 
-                         recursive = TRUE, full.names = TRUE)
-files_aspot = files_aspot[!str_detect(files_aspot, 'NS') & 
-                            !str_detect(files_aspot, 'HR') &
-                            !str_detect(files_aspot, 'TWJ') &
-                            !str_detect(files_aspot, 'togter')]
+# files_aspot = list.files(path_aspot, pattern = '*.csv', 
+#                          recursive = TRUE, full.names = TRUE)
+# files_aspot = files_aspot[!str_detect(files_aspot, 'NS') & 
+#                             !str_detect(files_aspot, 'HR') &
+#                             # !str_detect(files_aspot, 'TWJ') &
+#                             !str_detect(files_aspot, 'togter')]
 files_aspot_bats = list.files(path_aspot_bats, pattern = '*.csv', 
                               recursive = TRUE, full.names = TRUE)
 files_aspot_bats = files_aspot_bats[!str_detect(files_aspot_bats, 'NS') & 
                                       !str_detect(files_aspot_bats, 'HR') &
-                                      !str_detect(files_aspot_bats, 'TWJ') &
+                                      # !str_detect(files_aspot_bats, 'TWJ') &
                                       !str_detect(files_aspot_bats, 'togter')]
 # Read all files
 summary = files_summaries |>
   lapply(read.csv) |> bind_rows()
 summary_detections = files_detections |>
   lapply(read.csv) |> bind_rows()
-summary_aspot = files_aspot |>
-  lapply(read.csv) |> bind_rows()
+# summary_aspot = files_aspot |>
+#   lapply(read.csv) |> bind_rows()
 summary_aspot_bats = files_aspot_bats |>
   lapply(read.csv) |> bind_rows()
 
@@ -115,6 +115,8 @@ summary$station = ifelse(summary$station == 'STADILOE', 'STADILØ',
 summary$station = ifelse(summary$station == 'BLAAVAND', 'BLÅVAND', 
                          summary$station) 
 summary$station = ifelse(summary$station == 'FANO', 'FANØ', 
+                         summary$station) 
+summary$station = ifelse(summary$station == 'TWJ-08', 'MANDØ', 
                          summary$station) 
 
 summary_detections$station = 
@@ -174,66 +176,69 @@ summary_detections$station =
 summary_detections$station = 
   ifelse(summary_detections$station == 'FANO', 'FANØ', 
          summary_detections$station) 
+summary_detections$station = 
+  ifelse(summary_detections$station == 'TWJ-08', 'MANDØ', 
+         summary_detections$station) 
 
-summary_aspot$station = summary_aspot$station |>
-  toupper() 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'KAMMER', 'KAMMERSLUSEN', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND-BALLUM', 'BALLUM', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND-MANDØ', 'MANDØ', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND1', 'KAMMERSLUSEN', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND10-LOT1', 'REJSBY', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND2', 'BLÅVAND', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND3', 'SKJERN', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND4', 'STADILØ', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND5', 'HUSBY', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND6-LOT1', 'BALLUM', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND7-LOT1', 'MANDØ', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND8-LOT1', 'NYMINDE', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'LAND9-LOT1', 'FANØ', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'MANDO', 'MANDØ', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'NYMND-PLTG', 'NYMINDE', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'ROEMOE', 'RØMØ', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'STADILOE', 'STADILØ', 
-         summary_aspot$station) 
-summary_aspot$station =
-  ifelse(summary_aspot$station == 'BLAAVAND', 'BLÅVAND', 
-         summary_aspot$station) 
-summary_aspot$station = 
-  ifelse(summary_aspot$station == 'FANO', 'FANØ', 
-         summary_aspot$station) 
+# summary_aspot$station = summary_aspot$station |>
+#   toupper() 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'KAMMER', 'KAMMERSLUSEN', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND-BALLUM', 'BALLUM', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND-MANDØ', 'MANDØ', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND1', 'KAMMERSLUSEN', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND10-LOT1', 'REJSBY', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND2', 'BLÅVAND', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND3', 'SKJERN', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND4', 'STADILØ', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND5', 'HUSBY', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND6-LOT1', 'BALLUM', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND7-LOT1', 'MANDØ', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND8-LOT1', 'NYMINDE', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'LAND9-LOT1', 'FANØ', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'MANDO', 'MANDØ', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'NYMND-PLTG', 'NYMINDE', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'ROEMOE', 'RØMØ', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'STADILOE', 'STADILØ', 
+#          summary_aspot$station) 
+# summary_aspot$station =
+#   ifelse(summary_aspot$station == 'BLAAVAND', 'BLÅVAND', 
+#          summary_aspot$station) 
+# summary_aspot$station = 
+#   ifelse(summary_aspot$station == 'FANO', 'FANØ', 
+#          summary_aspot$station) 
 
 summary_aspot_bats$station = summary_aspot_bats$station |>
   toupper() |> 
@@ -245,7 +250,7 @@ summary_aspot_bats$station =
   ifelse(summary_aspot_bats$station == 'FANOE', 'FANØ', 
          summary_aspot_bats$station) 
 summary_aspot_bats$station = 
-  ifelse(summary_aspot_bats$station == 'MANDO', 'MANDØ', 
+  ifelse(summary_aspot_bats$station == 'MANDOE', 'MANDØ', 
          summary_aspot_bats$station) 
 summary_aspot_bats$station = 
   ifelse(summary_aspot_bats$station == 'ROEMOE', 'RØMØ', 
@@ -257,80 +262,100 @@ summary_aspot_bats$station =
 # Plot
 unique_stations = summary_detections$station |> unique() |> 
   sort(decreasing = TRUE)
+unique_stations = unique_stations[unique_stations != 'SKAGEN']
 trans_stations = seq_along(unique_stations)
 names(trans_stations) = unique_stations
 ## create colour gradient
-colfunc = colorRampPalette(c('#FAD7A0', '#0B5345'))
+colfunc = colorRampPalette(c('#EAEDED', '#5F6A6A'))
 cols = colfunc(max(summary$n))
-for(year in c(2023, 2024)){
-  pdf(sprintf('%s/%s_land.pdf', path_png, str_replace(year, ' ', '_')),
-      width = 40, height = 12) # , units = 'in', res = 1000
-  par(mar = c(5, 12, 3, 1))
-  ## subset per year and adjust xlims
-  if(year == 2023){
-    sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') <= 
-                          as.Date('2023-12-31')),]
-    sub_detections = 
-      summary_detections[which(as.Date(summary_detections$DATE, 
-                                       format = '%Y-%m-%d') <= 
-                                 as.Date('2023-12-31')),]
-    sub_aspot = 
-      summary_aspot[which(as.Date(summary_aspot$DATE) <= 
-                            as.Date('2023-12-31')),]
-    sub_aspot_bats = 
-      summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) <= 
-                                 as.Date('2023-12-31')),]
-    xlim = as.Date(c('2023-01-01', '2023-12-31'))
-  } else {
-    sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') > 
-                          as.Date('2023-07-15')),]
-    sub_detections = 
-      summary_detections[which(as.Date(summary_detections$DATE, 
-                                       format = '%Y-%m-%d') > 
-                                 as.Date('2023-12-31')),]
-    sub_aspot = 
-      summary_aspot[which(as.Date(summary_aspot$DATE) > 
-                            as.Date('2023-12-31')),]
-    sub_aspot_bats = 
-      summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) > 
-                                 as.Date('2023-12-31')),]
-    xlim = as.Date(c('2024-01-01', '2024-12-31'))
-  }
-  ## create empty plot
-  plot(as.Date(sub$DATE, format = '%Y-%b-%d'),
-       trans_stations[sub$station],
-       ylim = c(min(trans_stations) - 0.5, max(trans_stations) + 0.5),
-       xlim = xlim,
-       xaxt = 'n', yaxt = 'n', type = 'n',
-       xlab = 'Dato', ylab = '', main = year)
-  mtext('Station', 2, 10)
-  ## add filled squares and colour by activity
-  for(i in seq_len(nrow(sub))){
-    points_custom(as.Date(sub$DATE[i], format = '%Y-%b-%d'),
-                  trans_stations[sub$station[i]], 
-                  my_shape1, col = cols[sub$n[i]])
-  }
-  ## add scaled dots for number detections
-  points(as.Date(sub_detections$DATE, format = '%Y-%m-%d'),
-         trans_stations[sub_detections$station] + 0.15, pch = 16, 
-         cex = log10(sub_detections$n)/4 + 0.1)
-  ## add scaled dots for number detections from aspot
-  points(as.Date(sub_aspot$DATE),
-         trans_stations[sub_aspot$station] - 0.15, pch = 16, 
-         cex = log10(sub_aspot$n)/4 + 0.1)
-  points(as.Date(sub_aspot_bats$DATE),
-         trans_stations[sub_aspot_bats$station] - 0.15, pch = 16, 
-         cex = log10(sub_aspot_bats$n)/4 + 0.1, col = '#28B463')
-  ## add axes
-  unique_months = unique(format(ymd(sub$DATE), '%Y-%m'))
-  axis(1, at = as.Date(paste0(unique_months, '-01')), 
-       labels = paste0(unique_months, '-01') |> str_sub(6, 10))
-  axis(1, at = as.Date(paste0(unique_months, '-10')), 
-       labels = paste0(unique_months, '-10') |> str_sub(6, 10))
-  axis(1, at = as.Date(paste0(unique_months, '-20')), 
-       labels = paste0(unique_months, '-20') |> str_sub(6, 10))
-  axis(2, trans_stations, names(trans_stations), las = 1)
-  dev.off() # close PNG
+png(sprintf('%sland.png', path_png),
+    width = 40, height = 11, units = 'in', res = 800)
+par(mar = c(5, 18, 1, 1), xaxs = 'i', yaxs = 'i')
+## subset per year and adjust xlims
+if(TRUE){
+  sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') < 
+                        as.Date('2024-04-10')),]
+  sub_detections = 
+    summary_detections[which(as.Date(summary_detections$DATE, 
+                                     format = '%Y-%m-%d') <
+                               as.Date('2024-04-10')),]
+  # sub_aspot = 
+  #   summary_aspot[which(as.Date(summary_aspot$DATE) < 
+  #                         as.Date('2024-04-10')),]
+  sub_aspot_bats = 
+    summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) < 
+                               as.Date('2024-04-10')),]
+  xlim = as.Date(c('2023-04-10', '2024-04-10'))
+} else {
+  sub = summary[which(as.Date(summary$DATE, format = '%Y-%b-%d') >= 
+                        as.Date('2023-07-15')),]
+  sub_detections = 
+    summary_detections[which(as.Date(summary_detections$DATE, 
+                                     format = '%Y-%m-%d') >= 
+                               as.Date('2023-12-31')),]
+  # sub_aspot = 
+  #   summary_aspot[which(as.Date(summary_aspot$DATE) >= 
+  #                         as.Date('2023-12-31')),]
+  sub_aspot_bats = 
+    summary_aspot_bats[which(as.Date(summary_aspot_bats$DATE) >= 
+                               as.Date('2023-12-31')),]
+  xlim = as.Date(c('2024-01-01', '2024-12-31'))
 }
+## remove two points from Mandø
+sub_detections = 
+  sub_detections[!(sub_detections$station == 'MANDØ' & 
+                     sub_detections$DATE < as.Date('2023-05-01')),]
+sub_aspot_bats = 
+  sub_detections[!(sub_aspot_bats$station == 'MANDØ' & 
+                     sub_aspot_bats$DATE < as.Date('2023-05-01')),]
+## create empty plot
+ymin = min(trans_stations) - 0.45
+ymax = max(trans_stations) + 0.45
+plot(as.Date(sub$DATE, format = '%Y-%b-%d'),
+     trans_stations[sub$station],
+     ylim = c(ymin, ymax),
+     xlim = xlim,
+     xaxt = 'n', yaxt = 'n', type = 'n',
+     xlab = '', ylab = '')
+mtext('Date', 1, 3.5, cex = 2.5)
+mtext('Station', 2, 16, cex = 2.5)
+## add shaded area for migration
+polygon(as.Date(c('2023-04-10', '2023-04-10', '2023-05-15','2023-05-15')),
+        c(ymin, ymax, ymax, ymin),
+        col = '#E8DAEF', border = NA)
+polygon(as.Date(c('2023-08-15', '2023-08-15', '2023-10-15','2023-10-15')),
+        c(ymin, ymax, ymax, ymin),
+        col = '#E8DAEF', border = NA)
+polygon(as.Date(c('2024-04-01', '2024-04-01', '2024-04-10','2024-04-10')),
+        c(ymin, ymax, ymax, ymin),
+        col = '#E8DAEF', border = NA)
+## add filled squares and colour by activity
+for(i in seq_len(nrow(sub))){
+  points_custom(as.Date(sub$DATE[i], format = '%Y-%b-%d'),
+                trans_stations[sub$station[i]], 
+                my_shape1, col = cols[sub$n[i]])
+}
+## add scaled dots for number detections
+points(as.Date(sub_detections$DATE, format = '%Y-%m-%d'),
+       trans_stations[sub_detections$station] + 0.15, pch = 16, 
+       cex = log10(sub_detections$n)/4 + 0.1)
+## add scaled dots for number detections from aspot
+# points(as.Date(sub_aspot$DATE),
+#        trans_stations[sub_aspot$station] - 0.15, pch = 16, 
+#        cex = log10(sub_aspot$n)/4 + 0.1)
+points(as.Date(sub_aspot_bats$DATE),
+       trans_stations[sub_aspot_bats$station] - 0.15, pch = 16, 
+       cex = 1.1, col = '#D68910')
+## add axes
+unique_months = unique(format(ymd(sub$DATE), '%Y-%m'))
+axis(1, at = as.Date(paste0(unique_months, '-01')), 
+     labels = paste0(unique_months, '-01'), las = 1,
+     cex.axis = 2)
+# axis(1, at = as.Date(paste0(unique_months, '-10')), 
+#      labels = paste0(unique_months, '-10'), las = 0)
+# axis(1, at = as.Date(paste0(unique_months, '-20')), 
+#      labels = paste0(unique_months, '-20'), las = 0.5)
+axis(2, trans_stations, names(trans_stations), las = 1, cex.axis = 2)
+dev.off() # close PNG
 
 
