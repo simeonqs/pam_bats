9fef7e139ba75eb38e67042a9ab3f7aa4423ab94
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-12-17 13:17:36 +0100

    Updating fetch_era5_monthly.py. Moving old version to 'old_versions'. New version works with token and downloads all variables of interest.

--- analysis/code/download_weather/fetch_era5_monthly.py
+++ analysis/code/download_weather/fetch_era5_monthly.py
@@ -1,26 +1,43 @@
 import cdsapi
+import os
 
-c = cdsapi.Client()
-years = [str(i) for i in range(2023, 2025)]
-months = [
-    '01', '02', '03',
-    '04', '05', '06',
-    '07', '08', '09',
-    '10', '11', '12',
-]
-#y = years[:]
-#m = months[:]
-for year in years:
-    for month in months:
+#variables to download
+era5_variable_array = ['total_precipitation', '2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind']
+variable_store_array = ['precip', 'temp', 'windu', 'windv']
+
+#path to store the data
+store_path = '/home/au472091/OneDrive/au/projects/pam_bats/analysis/data/weather/'
+
+if not os.path.exists(store_path):
+    print('The path does not exist. Please check the path to store the data:', store_path)
+    exit()
+
+c = cdsapi.Client(url="https://cds.climate.copernicus.eu/api", key="fbcf6d2f-6dd5-47b9-9990-b0679803e7b1")
+start_year = 2023
+start_month = 1 # works with the start year
+end_year = 2024 # included
+end_month = 11 # included, works with the end year
+
+#calculating the start and end year_month together
+start_year_month = start_year*100 + start_month
+end_year_month = end_year*100 + end_month
+
+year_month = start_year_month
+while year_month <= end_year_month:
+    year = str(int(year_month/100))
+    month = str(year_month%100).zfill(2)
+    print (year, month)
+
+    for variable_name, store_name in zip(era5_variable_array, variable_store_array):
+        file_to_store = store_path + store_name + '/era5_' + store_name + '_' + year + '_' + month + '.grib'
+        print(variable_name, file_to_store, ' downloading...')
         c.retrieve(
             'reanalysis-era5-single-levels',
             {
                 'product_type': 'reanalysis',
                 'format': 'grib',
-                'year': year
-                ,
-                'month': month
-                ,
+                'year': year,
+                'month': month,
                 'day': [
                     '01', '02', '03',
                     '04', '05', '06',
@@ -44,10 +61,14 @@ for year in years:
                     '18:00', '19:00', '20:00',
                     '21:00', '22:00', '23:00',
                 ],
-                'variable': '2m_temperature',
+                'variable': variable_name,
                 'area': [
                     80, -30, 30,
                     50,
                 ],
             },
-            '/home/au472091/OneDrive/au/projects/pam_bats/analysis/data/weather/temp/era5_temp_'+year+'_'+month+'.grib')
+            file_to_store)
+    # Incrementing the year_month
+    year_month += 1
+    if(year_month%100 == 13):
+        year_month += 88
