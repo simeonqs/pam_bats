36b1a15d594c96cd555774f874a759ad0db3fc82
au472091 <simeonqs@hotmail.com>
2025-06-03 09:07:45 +0200

    Updating get_missed_wavs.R.

--- analysis/code/get_missed_wavs.R
+++ analysis/code/get_missed_wavs.R
@@ -2,10 +2,11 @@ library(stringr)
 rm(list=ls()) 
 
 wav_directory = 
-  '/media/au472091/T7 Shield/temp/NS30_B_Fall2023/Data'
+  '/media/au472091/T7 Shield/NS12_B_Spring23/NS12_B_Spring23'
 prediction_directory = 
-  '/home/au472091/Documents/results_aspot/NS30_B_Fall2023/predict'
-output_directory = '/media/au472091/T7 Shield/efteraar/NS30_B_Fall2023/Data'
+  '/media/au472091/Samsung_T5/results_aspot/NS12_B_Spring23/predict'
+output_directory = 
+  '/media/au472091/T7 Shield/missed_land'
 
 wav_files = list.files(wav_directory, full.names = TRUE)
 pred_files = list.files(prediction_directory, full.names = TRUE)
