bd7000344ec24bd428fbb280d1ae7123b2d4ba4a
au472091 <simeonqs@hotmail.com>
2025-06-03 09:01:18 +0200

    Updating aspot_get_associated_sound_files.R. Updating paths.

--- analysis/code/aspot_get_associated_sound_files.R
+++ analysis/code/aspot_get_associated_sound_files.R
@@ -16,10 +16,11 @@ for(lib in libraries){
 rm(list=ls()) 
 
 # Paths 
-path_detections = 'aspot/models/m38/selection_tables_1000'
+path_detections = 
+  '/media/au472091/cd5d3443-6980-4c41-96c9-fdfa2b7c264e/selection_tables_1000'
 path_audio = 
-  '/media/au472091/T7 Shield/LOT_1_BØJER_DATA/T3-NS26_A_Spring23/Data'
-path_output = 'analysis/data/audio/from_runs'
+  '/media/au472091/cd5d3443-6980-4c41-96c9-fdfa2b7c264e/togter_all'
+path_output = 'analysis/data/audio/additional_noise_togter'
 
 # Run through files and copy
 detections = load.selection.tables(path_detections)
