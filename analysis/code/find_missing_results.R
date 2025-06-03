1a2b94e4c365f5602452924590ec4a39e436aa12
au472091 <simeonqs@hotmail.com>
2025-06-03 09:07:18 +0200

    Updating find_missing_results.R. Updating paths.

--- analysis/code/find_missing_results.R
+++ analysis/code/find_missing_results.R
@@ -8,10 +8,11 @@
 library(dplyr)
 library(stringr)
 
-paths_results = list.files('/home/au472091/Documents/new_results_aspot/boejer')
+paths_results = 
+  list.files('/media/au472091/T7_black/new_results_aspot/land')
 paths_dirs = read.table('analysis/data/all_paths_ERDA.txt', sep = ',')$V1
 
-paths_dirs = paths_dirs[str_detect(paths_dirs, 'LOT1_BØJER_DATA')]
+paths_dirs = paths_dirs[str_detect(paths_dirs, 'LOT1_LAND_DATA')]
 paths_dirs = paths_dirs[!basename(paths_dirs) %in% paths_results]
 write.table(paths_dirs |> as.data.frame(), 
             '~/Desktop/directories_to_download.txt', 
