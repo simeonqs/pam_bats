2b79dacc749def38d07f36e4c866827808da67cf
au472091 <simeonqs@hotmail.com>
2025-06-03 09:11:27 +0200

    Updating spatial_model_plot_detections_new.R. Updating paths. Adding Y2 data. Updating how bat presence is evaluated. Commenting out plot. Removing duplicates in not-monitored stations. Updating to leading zero for NS stations. Deviding n by two.

--- analysis/code/spatial_model_plot_detections_new.R
+++ analysis/code/spatial_model_plot_detections_new.R
@@ -17,14 +17,18 @@ rm(list=ls())
 
 # Paths 
 path_combined_data = 'analysis/results/combined_data.RData'
-path_png = 'analysis/results/spatial_model/map_detections_new.png'
-path_gis_out = 'analysis/results/spatial_model/bat_pam_results.shp'
+path_png = 'analysis/results/spatial_model/map_detections_Y1+Y2.png'
+path_gis_out = 'analysis/results/spatial_model/bat_pam_results_Y1+Y2.shp'
 
 # Load data
 load(path_combined_data)
 
+# Check if all observations have lat/long
+missing = which(is.na(dat_model$lat) | is.na(dat_model$long))
+if(length(missing) > 0) stop('Missing lat/long.')
+
 # Summarise data
-summary = dat[which(dat$n_bats > 0 & 
+summary = dat[which(!is.na(dat$species) & 
                 dat$offshore & 
                 dat$type_location != 'fugletogter'),] |> 
   group_by(station, type_location) |> 
@@ -51,51 +55,54 @@ trans_loc = c(HRIII = 'Windturbines',
               Buoys = 'Buoys')
 plot_dat$cat = paste(plot_dat$col, trans_loc[plot_dat$type_location]) |> 
   factor(levels = c('Present Buoys', 'Present Windturbines',
-                    'Missing Buoys', 'Missing Windturbines'))
-theme_set(theme_bw())
-world = ne_countries(scale = 'large', returnclass = 'sf')
-plot = ggplot(data = world) +
-  # geom_tile(aes(x = Long..E., y = Lat..N., fill = predicted_prob),
-  #           data = new_data_grid, alpha = 0.7) +
-  scale_fill_gradient(low = 'white', high = 'darkgreen') +
-  geom_sf(fill = 'antiquewhite') + 
-  geom_point(data = plot_dat, aes(x = long, y = lat, 
-                                  col = cat, size = n + 1,
-                                  pch = subset)) +
-  scale_color_manual(values = c('springgreen4', 'springgreen4',
-                                'darkred', 'darkred'),
-                     labels = c('Present at buoys', 
-                                'Present at windturbines',
-                                'Absent at buoys', 
-                                'Absent at windturbines')) +
-  annotation_scale(location = 'bl', width_hint = 0.5) +
-  annotation_north_arrow(location = 'bl', which_north = 'true', 
-                         pad_x = unit(0.4, 'in'), pad_y = unit(0.5, 'in'),
-                         style = north_arrow_fancy_orienteering) +
-  coord_sf(xlim = c(6.5, 9.5), ylim = c(55.5, 56.6)) + 
-  labs(x = 'Longitude', y = 'Latitude',
-       fill = 'Predicted probability',
-       size = 'Nights with bats',
-       col = 'Type and colour') +
-  scale_size(breaks = c(1, 9, 1, 2),
-             range = c(3, 8)) +
-  guides(
-    color = guide_legend(order = 1, 
-                         override.aes = list(pch = c(16, 17, 16, 17),
-                                             cex = 3),
-                         position = 'inside'),
-    size = guide_legend(order = 2, 
-                        override.aes = list(color = 'springgreen4',
-                                            pch = c(16, 16, 17, 17)),
-                        position = 'inside'),
-    pch = 'none'
-  ) +
-  theme(panel.background = element_rect(fill = 'aliceblue'),
-        legend.background = element_rect(fill = NA, color = NA), 
-        legend.key = element_rect(fill = NA, color = NA),
-        legend.justification.inside = c(0.92, 0.5))
-print(plot)
-ggsave(path_png, plot, width = 8, height = 5.7, units = 'in', dpi = 800)
+                    'Missing Buoys', 'Missing Windturbines',
+                    'Present OSS'))
+plot_dat$cat[plot_dat$station == 'platform'] = 'Present OSS'
+plot_dat$station[plot_dat$station == 'platform'] = 'OSS'
+# theme_set(theme_bw())
+# world = ne_countries(scale = 'large', returnclass = 'sf')
+# plot = ggplot(data = world) +
+#   # geom_tile(aes(x = Long..E., y = Lat..N., fill = predicted_prob),
+#   #           data = new_data_grid, alpha = 0.7) +
+#   scale_fill_gradient(low = 'white', high = 'darkgreen') +
+#   geom_sf(fill = 'antiquewhite') + 
+#   geom_point(data = plot_dat, aes(x = long, y = lat, 
+#                                   col = cat, size = n + 1,
+#                                   pch = subset)) +
+#   scale_color_manual(values = c('springgreen4', 'springgreen4',
+#                                 'darkred', 'darkred'),
+#                      labels = c('Present at buoys', 
+#                                 'Present at windturbines',
+#                                 'Absent at buoys', 
+#                                 'Absent at windturbines')) +
+#   annotation_scale(location = 'bl', width_hint = 0.5) +
+#   annotation_north_arrow(location = 'bl', which_north = 'true', 
+#                          pad_x = unit(0.4, 'in'), pad_y = unit(0.5, 'in'),
+#                          style = north_arrow_fancy_orienteering) +
+#   coord_sf(xlim = c(6.5, 9.5), ylim = c(55.5, 56.6)) + 
+#   labs(x = 'Longitude', y = 'Latitude',
+#        fill = 'Predicted probability',
+#        size = 'Nights with bats',
+#        col = 'Type and colour') +
+#   scale_size(breaks = c(1, 16, 1, 16),
+#              range = c(3, 8)) +
+#   guides(
+#     color = guide_legend(order = 1, 
+#                          override.aes = list(pch = c(16, 17, 16, 17),
+#                                              cex = 3),
+#                          position = 'inside'),
+#     size = guide_legend(order = 2, 
+#                         override.aes = list(color = 'springgreen4',
+#                                             pch = c(16, 16, 17, 17)),
+#                         position = 'inside'),
+#     pch = 'none'
+#   ) +
+#   theme(panel.background = element_rect(fill = 'aliceblue'),
+#         legend.background = element_rect(fill = NA, color = NA), 
+#         legend.key = element_rect(fill = NA, color = NA),
+#         legend.justification.inside = c(0.92, 0.5))
+# print(plot)
+# ggsave(path_png, plot, width = 8, height = 5.7, units = 'in', dpi = 800)
 
 ## Add non-bat stations for GIS map
 gis_dat = plot_dat[c('station', 'cat', 'lat', 'long', 'n')]
@@ -104,12 +111,22 @@ non_bat = locations_all_buoys[!locations_all_buoys$Position.ID %in%
                               c('Position.ID', 'Deployment.LAT_DD', 
                                 'Deployment.LON_DD')]
 colnames(non_bat) = c('station', 'lat', 'long')
+non_bat = non_bat[!duplicated(non_bat$station),]
 non_bat$cat = ifelse(str_detect(non_bat$station, 'HR'), 
                      'Non-bat windturbines', 
                      'Non-bat buoys')
 non_bat$n = 0
+non_bat = non_bat[!non_bat$station == 'NS6',]
 gis_dat = rbind(gis_dat, non_bat)
-gis_dat$n = gis_dat$n + 3
+gis_dat$n = gis_dat$n/2 + 3
+
+# Add leading zero and reorder
+gis_dat$station = ifelse(
+  grepl('^NS\\d$', gis_dat$station),
+  sub('^NS(\\d)$', 'NS0\\1', gis_dat$station),
+  gis_dat$station
+)
+gis_dat = gis_dat[order(gis_dat$station),]
 
 # Export data for GIS
 sf_object = st_as_sf(gis_dat, 
