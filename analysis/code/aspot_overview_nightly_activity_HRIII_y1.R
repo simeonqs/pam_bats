2399e1c65da566503d2790f77ba3c70f9144b02e
Simeon Q. Smeele <simeonqs@hotmail.com>
2025-06-26 15:38:29 +0200

    Updating aspot_overview_nightly_activity_HRIII_y1.R. Updating paths. Updating how presence bats is evaluated. Only including data from y1. Removing unused code. Making missin data white.

--- analysis/code/aspot_overview_nightly_activity_HRIII_y1.R
+++ analysis/code/aspot_overview_nightly_activity_HRIII_y1.R
@@ -16,29 +16,25 @@ rm(list=ls())
 
 # Paths
 path_combined_data = 'analysis/results/combined_data.RData'
-path_png = 'analysis/results/nightly_activity/nightly_activity_HRIII_new.png'
+path_png = 'analysis/results/nightly_activity/nightly_activity_HRIII_y1.png'
 
 # Load data and subset for boejer
 load(path_combined_data)
 dat = dat[which(dat$type_location == 'HRIII'),]
 
 # Open png
-png(path_png, 5, 3.5, units = 'in', res = 800)
+png(path_png, 5, 3.3, units = 'in', res = 800)
 par(mar = c(4, 4, 1, 1))
 
 # Subset
-sub_with_detections = dat[dat$n_bats > 0 & dat$offshore,]
+dat = dat[dat$type_location == 'HRIII' & dat$offshore,]
+dat = dat[which(dat$date < as.Date('2024-04-10')),]
+sub_with_detections = dat[!is.na(dat$species),]
 
 # Add species(complex)
 species_station = vapply(sub_with_detections$file_name, function(x){
   species_offshore$sp[species_offshore$Fil == x]
 }, character(1))
-
-# for(i in 1:nrow(sub_with_detections)){
-#   x = sub_with_detections$file_name[i]
-#   print(x)
-#   print(species_offshore$sp[species_offshore$Fil == x])
-# }
 
 # Make empty plot
 plot(NULL, 
@@ -50,6 +46,13 @@ axis.Date(side = 1, at = seq(as.Date('2023-05-01'),
                              by = 'month'), 
           labels = '')
 
+# Make shadow for night
+sun_sub = sun[sun$Date > as.Date('2023-04-09') &
+                sun$Date < as.Date('2024-04-10'),]
+polygon(x = c(as.Date(sun_sub$Date)+1, rev(as.Date(sun_sub$Date))),
+        y = c(sun_sub$rise_min, rev(sun_sub$set_min)),
+        col = '#212F3D', border = '#212F3D')
+
 # Mark missing dates
 all_dates = seq(from = as.Date('2023-04-10'),
                 to = as.Date('2024-04-10'),
@@ -58,14 +61,7 @@ missing_dates = all_dates[!all_dates %in% as.Date(dat$date)]
 for(d in missing_dates) 
   polygon(x = c(d-0.5, d+0.5),
           y = c(0, 1440),
-          col = '#BDC3C7', border = '#BDC3C7')
-
-# Make shadow for night
-sun_sub = sun[sun$Date > as.Date('2023-04-09') &
-                sun$Date < as.Date('2024-04-10'),]
-polygon(x = c(as.Date(sun_sub$Date)+1, rev(as.Date(sun_sub$Date))),
-        y = c(sun_sub$rise_min, rev(sun_sub$set_min)),
-        col = '#212F3D', border = '#212F3D')
+          col = 'white', border = 'white')
 
 # Plot points
 points(sub_with_detections$night_date, 
@@ -87,8 +83,8 @@ mtext('Month', 1, 2.8, cex = 1)
 
 # Print legend
 # plot.new()
-legend('bottomright', legend = (species |> str_replace('NVE', 'ENV'))[c(2, 3)], 
-       col = colours[c(2, 3)], pch = 16,
+legend('bottomright', legend = species[species %in% dat$species], 
+       col = colours[species %in% dat$species], pch = 16,
        cex = 0.75)
 
 # Close png
