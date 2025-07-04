6913fc6dbf1347158543f3e985db7a6392850e60
Simeon Q. Smeele <simeonqs@hotmail.com>
2025-07-04 11:58:11 +0200

    Updating find_missing_results.R. Updating paths and how files are listed.

--- analysis/code/find_missing_results.R
+++ analysis/code/find_missing_results.R
@@ -9,11 +9,12 @@ library(dplyr)
 library(stringr)
 
 paths_results = 
-  list.files('/media/au472091/data/new_results_aspot/land')
-paths_dirs = read.table('analysis/data/all_paths_ERDA.txt', sep = ',')$V1
+  Sys.glob('/media/au472091/data/new_results_aspot/*/*')
+paths_dirs = read.table('analysis/data/all_paths_ERDA_2025_06.txt', 
+                        sep = ',')$V1
 
-paths_dirs = paths_dirs[str_detect(paths_dirs, 'LOT1_LAND_DATA')]
-paths_dirs = paths_dirs[!basename(paths_dirs) %in% paths_results]
+# paths_dirs = paths_dirs[str_detect(paths_dirs, 'LOT1_LAND_DATA')]
+paths_dirs = paths_dirs[!basename(paths_dirs) %in% basename(paths_results)]
 write.table(paths_dirs |> as.data.frame(), 
             '~/Desktop/directories_to_download.txt', 
             row.names = FALSE, col.names = FALSE, quote = FALSE)
