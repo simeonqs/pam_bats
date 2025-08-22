344e1417bb317b4d06fed785406a5f6a0a3d2186
au472091 <simeonqs@hotmail.com>
2025-08-22 12:26:22 +0200

    Updating export_data_vattenfall.R. Adding buoy data as well.

--- analysis/code/export_data_vattenfall.R
+++ analysis/code/export_data_vattenfall.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: pam_bats  
 # Author: Simeon Q. Smeele
-# Description: Exports the data (HRIII - y1) for the Vattenfall project.  
+# Description: Exports the data from y1 for the Vattenfall project.  
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Clean R
@@ -9,12 +9,12 @@ rm(list=ls())
 
 # Paths 
 path_combined_data = 'analysis/results/combined_data.RData'
-path_out = 'analysis/results/dat_HRIII.RData'
+path_out = 'analysis/results/dat_model_NSI_y1.RData'
 
 # Load data
 load(path_combined_data)
-dat_HRIII = dat_model[dat_model$subset == 'Windturbines' &
-                        dat_model$date <= as.Date('2024-04-10'),]
+dat_model_NSI_y1 = dat_model[dat_model$subset %in% c('Windturbines', 'HRIII') &
+                               dat_model$date <= as.Date('2024-04-10'),]
 
 # Save
-save(dat_HRIII, file = path_out)
+save(dat_model_NSI_y1, file = path_out)
